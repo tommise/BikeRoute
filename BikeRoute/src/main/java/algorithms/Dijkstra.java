a387c8cdd85486107c75c8dbf6b931d38c27ab57
tommise <tommiek83@gmail.com>
2020-09-11 20:32:02 +0300

    Dijkstran toteutus lisätty

---
+++ BikeRoute/src/main/java/algorithms/Dijkstra.java
@@ -0,0 +1,70 @@
+package algorithms;
+
+import components.Kaari;
+import components.Solmu;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Dijkstran algoritmi hyödyntäen PriorityQueta
+ * - Lopussa lyhyimmän reitin tallettaminen listaan
+ */
+
+public class Dijkstra {
+    
+    PriorityQueue<Solmu> pq;
+    
+    public List<Solmu> etsiLyhyinReitti(Solmu lahtoSolmu, Solmu tavoiteSolmu) {
+        
+        lahtoSolmu.setMinimiEtaisyys(0);
+        pq = new PriorityQueue<>();
+        pq.add(lahtoSolmu);
+
+        while (!pq.isEmpty()) {
+            Solmu solmu = pq.poll();
+
+            for (Kaari kaari : solmu.getKaaret()) {
+                Solmu s = kaari.getLoppu();
+                double uusiMinimiEtaisyys = solmu.getMinimiEtaisyys() + kaari.getEtaisyys();
+
+                if (uusiMinimiEtaisyys < s.getMinimiEtaisyys()) {
+                    pq.remove(solmu);
+                    s.setEdellinenSolmu(solmu);
+                    s.setMinimiEtaisyys(uusiMinimiEtaisyys);
+                    pq.add(s);
+                }
+            }
+        }
+        
+        List<Solmu> reitti = new ArrayList<>();
+
+        for (Solmu solmu = tavoiteSolmu; solmu != null; solmu = solmu.getEdellinenSolmu()) {
+            reitti.add(solmu);
+        }
+
+        reitti = kaannaReitti(reitti);
+        
+        return reitti;        
+    }
+    
+    public List<Solmu> kaannaReitti(List<Solmu> reitti) { 
+        List<Solmu> kaannettyReitti = new ArrayList<>(); 
+        
+        for (int i = reitti.size()-1; i >= 0; i--) { 
+            kaannettyReitti.add(reitti.get(i)); 
+        } 
+        
+        return kaannettyReitti; 
+    } 
+    
+    public void tulostaReitti(List<Solmu> reitti) {
+        
+        for (Solmu solmu : reitti) {
+            System.out.println(solmu.getNimi() + " " + solmu.getMinimiEtaisyys() + "m");
+        }
+        
+        System.out.println("");
+        System.out.println("Kokonaisreitti yhteensä: " + reitti.get(reitti.size() - 1).getMinimiEtaisyys() + "m");
+    }
+}
