80f6e825fe44c35077d30f28c5a59df1f7ae6d2f
tommise <tommiek83@gmail.com>
2020-09-18 14:37:42 +0300

    Oman ArrayListin implementointi, javadoc

--- BikeRoute/src/main/java/algorithms/Dijkstra.java
+++ BikeRoute/src/main/java/algorithms/Dijkstra.java
@@ -2,23 +2,20 @@ package algorithms;
 
 import components.Kaari;
 import components.Solmu;
-import java.util.ArrayList;
-import java.util.List;
+import data_structures.ArrayList;
+
 import java.util.PriorityQueue;
 
 /**
- * Dijkstran algoritmi hyödyntäen PriorityQueta
- * - Lopussa lyhyimmän reitin tallettaminen listaan
+ * Dijkstran algoritmi hyödyntäen PriorityQueta sekä lyhyimmän reitin tallettaminen listaan
  */
 
 public class Dijkstra {
     
-    PriorityQueue<Solmu> pq;
-    
-    public List<Solmu> etsiLyhyinReitti(Solmu lahtoSolmu, Solmu tavoiteSolmu) {
+    public void etsi(Solmu lahtoSolmu, Solmu tavoiteSolmu) {
         
         lahtoSolmu.setMinimiEtaisyys(0);
-        pq = new PriorityQueue<Solmu>();
+        PriorityQueue<Solmu> pq = new PriorityQueue<Solmu>();
         pq.add(lahtoSolmu);
 
         while (!pq.isEmpty()) {
@@ -37,47 +34,28 @@ public class Dijkstra {
             }
         }
         
-        List<Solmu> reitti = new ArrayList<Solmu>();
+        luoReitti(tavoiteSolmu);
+    }
+
+    /**
+     * Luodaan reitti saadun tuloksen perusteella
+     * @param tavoiteSolmu solmu johon reitti päättyy
+     * @return lyhyin reitti lista muodossa
+     */    
+    
+    public ArrayList<Solmu> luoReitti(Solmu tavoiteSolmu) {
+        ArrayList<Solmu> reitti = new ArrayList<Solmu>();
 
         for (Solmu solmu = tavoiteSolmu; solmu != null; solmu = solmu.getEdellinenSolmu()) {
             reitti.add(solmu);
         }
-
-        reitti = kaannaReitti(reitti);
         
-        return reitti;        
-    }
-    
-    /**
-     * Kääntää listarakenteen toisinpäin samaan tapaan kuin Collections.reverse()
-     * 
-     * @param reitti
-     * @return käännetty reitti Listarakenteessa
-     */
-    
-    public List<Solmu> kaannaReitti(List<Solmu> reitti) { 
-        List<Solmu> kaannettyReitti = new ArrayList<Solmu>(); 
+        ArrayList<Solmu> kaannettyReitti = new ArrayList<Solmu>(); 
         
         for (int i = reitti.size()-1; i >= 0; i--) { 
             kaannettyReitti.add(reitti.get(i)); 
         } 
         
         return kaannettyReitti; 
-    } 
-    
-    /**
-     * Tulostaa reitin käyttöliittymälle
-     * 
-     * @param reitti tulostettava reitti
-     */
-    
-    public void tulostaReitti(List<Solmu> reitti) {
-        
-        for (Solmu solmu : reitti) {
-            System.out.println(solmu.getNimi() + " " + solmu.getMinimiEtaisyys() + "m");
-        }
-        
-        System.out.println("");
-        System.out.println("Kokonaisreitti yhteensä: " + reitti.get(reitti.size() - 1).getMinimiEtaisyys() + "m");
     }
 }
