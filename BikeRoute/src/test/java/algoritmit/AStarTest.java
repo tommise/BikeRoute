c385823e5e2f024c8536e09f507136623afd825c
tommise <tommiek83@gmail.com>
2020-10-09 18:25:25 +0300

    Lisätty uusia reittejä testattavaksi

--- BikeRoute/src/test/java/algoritmit/AStarTest.java
+++ BikeRoute/src/test/java/algoritmit/AStarTest.java
@@ -1,38 +1,82 @@
 
-package algorithms;
+package algoritmit;
 
-import static io.VerkonRakentaja.luoTestiVerkko;
+import io.VerkonRakentaja;
+import java.util.List;
 import static org.junit.Assert.*;
 
-import components.Solmu;
-import components.Verkko;
+import komponentit.Solmu;
+import komponentit.Verkko;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import tietorakenteet.ArrayList;
+
 
 public class AStarTest {
 
     AStar astar;    
     Verkko verkko;
     double epsilon = 0.001;
+    VerkonRakentaja rakentaja; 
     
     @Before
     public void setUp() {
         this.astar = new AStar();
-        this.verkko = luoTestiVerkko();
+        
+        this.rakentaja = new VerkonRakentaja();
+        this.verkko = rakentaja.luoTestiVerkko();
     } 
     
     @Test
-    public void palauttaaOikeinLyhyimmanPolun() {
-        Solmu alku = verkko.getSolmuByNimi("Rantatie");
-        Solmu loppu = verkko.getSolmuByNimi("Keskustie");
+    public void palauttaaOikeinLyhyimmanPolunReitti1() {
+        
+        List<Solmu> solmut = verkko.getSolmut();
+        
+        Solmu alku = solmut.get(0);
+        Solmu loppu = solmut.get(solmut.size() - 1);
         
         astar.etsi(alku, loppu);
-        java.util.ArrayList<Solmu> reitti = astar.luoReitti(loppu);
+        ArrayList<Solmu> reitti = astar.luoReitti(loppu);
         
-        double reitinKokonaisPituus = reitti.get(reitti.size() - 1).getG();
-        double reitinTavoitePituus = 325.0;
+        double lyhyinPolkuAStar = reitti.get(reitti.size() - 1).getG();
+        double reitinOikeaPituus = 786.9615717880192;
         
-        assertEquals(reitinKokonaisPituus, reitinTavoitePituus, epsilon);
+        assertEquals(lyhyinPolkuAStar, reitinOikeaPituus, epsilon);
     }
+    
+    @Test
+    public void palauttaaOikeinLyhyimmanPolunReitti2() {
+        
+        List<Solmu> solmut = verkko.getSolmut();
+        
+        Solmu alku = solmut.get(3);
+        Solmu loppu = solmut.get(12);
+        
+        astar.etsi(alku, loppu);
+        ArrayList<Solmu> reitti = astar.luoReitti(loppu);
+        
+        double lyhyinPolkuAStar = reitti.get(reitti.size() - 1).getG();
+        double reitinOikeaPituus = 375.823394569141;
+        
+        assertEquals(lyhyinPolkuAStar, reitinOikeaPituus, epsilon);
+    }
+
+    @Test
+    public void palauttaaOikeinLyhyimmanPolunReitti3() {
+        
+        List<Solmu> solmut = verkko.getSolmut();
+        
+        Solmu alku = solmut.get(10);
+        Solmu loppu = solmut.get(18);
+        
+        astar.etsi(alku, loppu);
+        ArrayList<Solmu> reitti = astar.luoReitti(loppu);
+        
+        double lyhyinPolkuAStar = reitti.get(reitti.size() - 1).getG();
+        double reitinOikeaPituus = 293.860036810901;
+        
+        assertEquals(lyhyinPolkuAStar, reitinOikeaPituus, epsilon);
+    }    
 }
--- BikeRoute/src/test/java/algoritmit/DijkstraTest.java
+++ BikeRoute/src/test/java/algoritmit/DijkstraTest.java
@@ -1,37 +1,82 @@
 
-package algorithms;
+package algoritmit;
 
-import static io.VerkonRakentaja.luoTestiVerkko;
+import io.VerkonRakentaja;
+import java.util.List;
 import static org.junit.Assert.*;
 
-import components.Solmu;
-import components.Verkko;
+import komponentit.Solmu;
+import komponentit.Verkko;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import tietorakenteet.ArrayList;
 
 public class DijkstraTest {
     
     Verkko verkko;
     Dijkstra dijkstra;
     double epsilon = 0.001;
+    VerkonRakentaja rakentaja;
     
     @Before
     public void setUp() {
         this.dijkstra = new Dijkstra();
-        this.verkko = luoTestiVerkko();
+        
+        this.rakentaja = new VerkonRakentaja();
+        this.rakentaja.luoTestiVerkko();
+        this.verkko = rakentaja.luoTestiVerkko();
     } 
     
     @Test
-    public void palauttaaOikeinLyhyimmanPolun() {
-        Solmu lahto = verkko.getSolmuByNimi("Rantatie");
-        Solmu tavoite = verkko.getSolmuByNimi("Keskustie");
+    public void palauttaaOikeinLyhyimmanPolunReitti1() {
         
-        dijkstra.etsi(lahto, tavoite);
-        java.util.ArrayList<Solmu> reitti = dijkstra.luoReitti(tavoite);
+        List<Solmu> solmut = verkko.getSolmut();
         
-        double reitinKokonaisPituus = reitti.get(reitti.size() - 1).getMinimiEtaisyys();
-        double reitinTavoitePituus = 325.0;
+        Solmu alku = solmut.get(0);
+        Solmu loppu = solmut.get(solmut.size() - 1);
         
-        assertEquals(reitinKokonaisPituus, reitinTavoitePituus, epsilon);
+        dijkstra.etsi(alku, loppu);
+        ArrayList<Solmu> reitti = dijkstra.luoReitti(loppu);
+        
+        double lyhyinPolkuDijkstra = reitti.get(reitti.size() - 1).getMinimiEtaisyys();
+        double reitinOikeaPituus = 786.9615717880192;
+        
+        assertEquals(lyhyinPolkuDijkstra, reitinOikeaPituus, epsilon);
+    }    
+    
+    @Test
+    public void palauttaaOikeinLyhyimmanPolunReitti2() {
+        
+        List<Solmu> solmut = verkko.getSolmut();
+        
+        Solmu alku = solmut.get(3);
+        Solmu loppu = solmut.get(12);
+        
+        dijkstra.etsi(alku, loppu);
+        ArrayList<Solmu> reitti = dijkstra.luoReitti(loppu);
+        
+        double lyhyinPolkuDijkstra = reitti.get(reitti.size() - 1).getMinimiEtaisyys();
+        double reitinOikeaPituus = 375.823394569141;
+        
+        assertEquals(lyhyinPolkuDijkstra, reitinOikeaPituus, epsilon);
+    } 
+
+    @Test
+    public void palauttaaOikeinLyhyimmanPolunReitti3() {
+        
+        List<Solmu> solmut = verkko.getSolmut();
+        
+        Solmu alku = solmut.get(10);
+        Solmu loppu = solmut.get(18);
+        
+        dijkstra.etsi(alku, loppu);
+        ArrayList<Solmu> reitti = dijkstra.luoReitti(loppu);
+        
+        double lyhyinPolkuDijkstra = reitti.get(reitti.size() - 1).getMinimiEtaisyys();
+        double reitinOikeaPituus = 293.860036810901;
+        
+        assertEquals(lyhyinPolkuDijkstra, reitinOikeaPituus, epsilon);
     }
 }
